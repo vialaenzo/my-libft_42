3d741600b33b17c7cebb5ef8c7c0a0d34470f30e
Slapze <viala.enzo@gmail.com>
2024-04-23 23:29:42 +0200

    test

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,12 +12,19 @@
 
 #include "ft_lib.h"
 
-size_t  ft_strlen(const char *s)
+size_t	ft_strlen(const char *s)
 {
     size_t	i;
 
     i = 0;
     while (s[i])
-        i++;
+	i++;
     return (i);
+}
+
+int     main(void)
+{
+    char        *str = "Hello World";
+    printf("%zu\n", ft_strlen(str));
+    return (0);
 }
